d8be89b996fa0c083b03d222a3f047e23c5d23bf
Stosbaby <57594354+Stosbaby@users.noreply.github.com>
2024-03-23 23:38:22 +0100

    ça débite ça débite

--- pagejeux/ds/assets/js/search.js
+++ pagejeux/ds/assets/js/search.js
@@ -2,15 +2,18 @@
 const searchInput = document.getElementById('searchInput');
 const articles = document.querySelectorAll('.item');
 
-searchInput.addEventListener('input', function() {
-    const searchText = this.value.toLowerCase();
-
-    articles.forEach(article => {
-        const articleText = article.textContent.toLowerCase();
-        if (articleText.includes(searchText)) {
-            article.style.display = 'block';
-        } else {
-            article.style.display = 'none';
-        }
-    });
+searchInput.addEventListener('input', function () {
+  const searchText = this.value.toLowerCase();
+  
+  articles.forEach(article => {
+    const articleText = article.textContent.toLowerCase();
+    
+    if (articleText.includes(searchText)) {
+      article.style.display = 'block';
+      // Modifier la couleur du texte en noir en utilisant un sélecteur CSS spécifique
+      article.style.color = 'black';
+    } else {
+      article.style.display = 'none';
+    }
+  });
 });
