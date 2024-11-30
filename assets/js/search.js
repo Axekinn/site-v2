5648ecdf65f8b84b57ce59aa964f7777f2da7763
Axekinn <axel.mendes@esicad.org>
2024-11-30 18:33:02 +0100

    ajout possibilité de faire des espaces dans la barre de recherche + embelissage de la barre de recherche

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,17 +1,16 @@
-// Code JavaScript pour la fonctionnalité de recherche
+// Code JavaScript pour la fonctionnalité de recherche améliorée
 const searchInput = document.getElementById('searchInput');
 const articles = document.querySelectorAll('.item');
 
 searchInput.addEventListener('input', function () {
-  const searchText = this.value.toLowerCase();
+  const searchTerms = this.value.toLowerCase().split(' ').filter(term => term);
   
   articles.forEach(article => {
     const articleText = article.textContent.toLowerCase();
-    
-    if (articleText.includes(searchText)) {
+    const matches = searchTerms.every(term => articleText.includes(term));
+
+    if (matches) {
       article.style.display = 'block';
-      // Modifier la couleur du texte en noir en utilisant un sélecteur CSS spécifique
-      article.style.color = 'black';
     } else {
       article.style.display = 'none';
     }
