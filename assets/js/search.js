b89bcd8d788bfa693c852b876fb53399193bea17
Axekinn <axel.mendes@esicad.org>
2025-01-22 09:51:33 +0100

    Correction + ajout de jsp quoi

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,11 +3,11 @@ const searchInput = document.getElementById('searchInput');
 const articles = document.querySelectorAll('.item');
 
 searchInput.addEventListener('input', function () {
-  const searchTerms = this.value.toLowerCase().split(' ').filter(term => term);
-  
+  const searchTerm = this.value.toLowerCase().trim();
+
   articles.forEach(article => {
     const articleText = article.textContent.toLowerCase();
-    const matches = searchTerms.every(term => articleText.includes(term));
+    const matches = articleText.includes(searchTerm);
 
     if (matches) {
       article.style.display = 'block';
