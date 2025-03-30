954cf418379bc424562da946b6fbe7c3617c9c11
Axekinn <axel.mendes@esicad.org>
2025-03-30 22:39:02 +0200

    Ajout jeux switch seconde partie

--- site-v3/assets/chunks/nintendo/switch-games.js
+++ site-v3/assets/chunks/nintendo/switch-games.js
@@ -102,33 +102,483 @@ const switchGames = [
     "image": "../../../assets/images/nintendo/Demon-Slayer-Kimetsu-no-Yaiba-The-Hinokami-Chronicles-box.png"
   },
   {
-    "title": "Diablo II: Resurrected",
-    "details": "Version 1.0.27.0 + Offline Crack",
-    "url": "../../../nintendo/diablo-ii.html",
-    "image": "../../../assets/images/nintendo/diablo-ii-box.jpg"
-  },
-  {
-    "title": "Donkey Kong Country Returns HD",
-    "details": "Version 1.0",
-    "url": "../../../nintendo/donkey-kong-country-returns-hd.html",
-    "image": "../../../assets/images/nintendo/donkey-kong-country-returns-hd-box.jpg"
-  },
-  {
-    "title": "Doom",
-    "details": "Version 1.0 + Doom II",
-    "url": "../../../nintendo/doom.html",
-    "image": "../../../assets/images/nintendo/doom-box.jpg"
+    "title": "Doom I + Doom II",
+    "details": "Version 1.0.2 + DLC",
+    "url": "../../../nintendo/doom-I-+-II.html",
+    "image": "../../../assets/images/nintendo/doom-I-+-II-box.jpg"
   },
   {
     "title": "Dragon Ball Z Kakarot",
-    "details": "Version 1.3.7 + DLC",
-    "url": "../../../nintendo/dragon-ball-z.html",
+    "details": "Version 1.51 + DLC",
+    "url": "../../../nintendo/dragon-ball-z-kakarot.html",
     "image": "../../../assets/images/nintendo/dragon-ball-z-kakarot-box.jpg"
   },
   {
-    "title": "Nintendo Switch Sports",
+    "title": "Dragon Quest III HD-2D Remake",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/Dragon-Quest-III-HD-2D-Remake.html",
+    "image": "../../../assets/images/nintendo/Dragon-Quest-III-HD-2D-Remake-box.png"
+  },
+  {
+    "title": "Dragon Quest Monsters: The Dark Prince",
+    "details": "Version 1.0.6",
+    "url": "../../../nintendo/Dragon-Quest-Monsters-The-Dark-Prince-Switch.html",
+    "image": "../../../assets/images/nintendo/Dragon-Quest-Monsters-The-Dark-Prince-Switch-box.jpg"
+  },
+  {
+    "title": "F1 Manager 2024",
+    "details": "Version 1.3 + Deluxe Upgrade Pack DLC",
+    "url": "../../../nintendo/F1-Manager-2024.html",
+    "image": "../../../assets/images/nintendo/F1-Manager-2024-box.jpeg"
+  },
+  {
+    "title": "FC 24",
+    "details": "Version 786432",
+    "url": "../../../nintendo/Fc-24.html",
+    "image": "../../../assets/images/nintendo/Fc-24-box.jpg"
+  },
+  {
+    "title": "FC 25",
+    "details": "Version 786432",
+    "url": "../../../nintendo/Fc-25.html",
+    "image": "../../../assets/images/nintendo/Fc-25-box.png"
+  },
+  {
+    "title": "FIFA 23",
+    "details": "Version 1.0.3",
+    "url": "../../../nintendo/fifa-23.html",
+    "image": "../../../assets/images/nintendo/fifa-23-box.jpg"
+  },
+  {
+    "title": "FIFA 22",
+    "details": "Version 1.0.5",
+    "url": "../../../nintendo/fifa-22.html",
+    "image": "../../../assets/images/nintendo/fifa-22-box.jpeg"
+  },
+  {
+    "title": "Fire Emblem Engage",
+    "details": "Version 2.0.0 + DLC",
+    "url": "../../../nintendo/fire-emblem-engage.html",
+    "image": "../../../assets/images/nintendo/fire-emblem-engage-box.jpg"
+  },
+  {
+    "title": "Fire Emblem: Three Houses",
+    "details": "Version 1.2.0 + DLC",
+    "url": "../../../nintendo/fire-emblem-three-houses.html",
+    "image": "../../../assets/images/nintendo/fire-emblem-three-houses-box.jpg"
+  },
+  {
+    "title": "Fire Emblem Warriors",
+    "details": "Version 1.5.0 + DLC",
+    "url": "../../../nintendo/fire-emblem-warriors.html",
+    "image": "../../../assets/images/nintendo/fire-emblem-warriors-box.jpg"
+  },
+  {
+    "title": "Five Nights at Freddy's Core Collection",
+    "details": "Version 1.0",
+    "url": "../../../nintendo/five-nights-at-freddys-core-collection.html",
+    "image": "../../../assets/images/nintendo/five-nights-at-freddys-core-collection-box.PNG"
+  },
+  {
+    "title": "Goat Simulator 3",
+    "details": "Version 1.0.71 + DLC",
+    "url": "../../../nintendo/goat-simulator-3.html",
+    "image": "../../../assets/images/nintendo/goat-simulator-3-box.jpg"
+  },
+  {
+    "title": "Guilty Gear -Strive- Nintendo Switch Edition",
+    "details": "Version 1.02",
+    "url": "../../../nintendo/guilty-gear-strive-nintendo-switch-edition.html",
+    "image": "../../../assets/images/nintendo/guilty-gear-strive-nintendo-switch-edition-box.webp"
+  },
+  {
+    "title": "Harvest Moon: Un Monde à Cultiver",
+    "details": "Version 1.6.0 + DLC",
+    "url": "../../../nintendo/harvest-moon-un-monde-a-cultiver.html",
+    "image": "../../../assets/images/nintendo/harvest-moon-un-monde-a-cultiver-box.jpg"
+  },
+  {
+    "title": "Hogwarts Legacy: Deluxe Edition",
+    "details": "Version 1.0.4 + DLC",
+    "url": "../../../nintendo/hogwarts-legacy-deluxe-edition.html",
+    "image": "../../../assets/images/nintendo/hogwarts-legacy-deluxe-edition-box.jpg"
+  },
+  {
+    "title": "Hollow Knight",
+    "details": "Version 1.4.3.2b + DLC",
+    "url": "../../../nintendo/hollow-knight.html",
+    "image": "../../../assets/images/nintendo/hollow-knight-box.jpg"
+  },
+  {
+    "title": "Hyrule Warriors: L'ère du fléau",
+    "details": "Version 1.3.0 + DLC",
+    "url": "../../../nintendo/hyrule-warriors-age-of-calamity.html",
+    "image": "../../../assets/images/nintendo/hyrule-warriors-age-of-calamity-box.jpg"
+  },
+  {
+    "title": "Hyrule Warriors: Definitive Edition",
+    "details": "Version 1.0.01",
+    "url": "../../../nintendo/hyrule-warriors-definitive-edition.html",
+    "image": "../../../assets/images/nintendo/hyrule-warriors-definitive-edition-box.jpg"
+  },
+  {
+    "title": "Inazuma Eleven Victory Road",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/inazuma-eleven-victory-road.html",
+    "image": "../../../assets/images/nintendo/inazuma-eleven-victory-road-box.jpg"
+  },
+  {
+    "title": "It Takes Two",
+    "details": "Version 1.0.2 + DLC",
+    "url": "../../../nintendo/it-takes-two.html",
+    "image": "../../../assets/images/nintendo/it-takes-two-box.jpg"
+  },
+  {
+    "title": "Jump Force",
+    "details": "Version 1.0.8 + DLC",
+    "url": "../../../nintendo/jump-force.html",
+    "image": "../../../assets/images/nintendo/jump-force-box.jpg"
+  },
+  {
+    "title": "Kirby's Return to Dream Land Deluxe",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/kirbys-return-to-dream-land-deluxe.html",
+    "image": "../../../assets/images/nintendo/kirbys-return-to-dream-land-deluxe-box.jpg"
+  },
+  {
+    "title": "Kirby et le monde oublié",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/kirby-et-le-monde-oublie.html",
+    "image": "../../../assets/images/nintendo/kirby-et-le-monde-oublie-box.png"
+  },
+  {
+    "title": "Kirby Star Allies",
+    "details": "Version 4.0.0",
+    "url": "../../../nintendo/kirby-star-allies.html",
+    "image": "../../../assets/images/nintendo/kirby-star-allies-box.jpg"
+  },
+  {
+    "title": "L'atelier du jeu vidéo",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/atelier-du-jeu-video.html",
+    "image": "../../../assets/images/nintendo/atelier-du-jeu-video-box.jpg"
+  },
+  {
+    "title": "Lego City Undercover",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/lego-city-undercover.html",
+    "image": "../../../assets/images/nintendo/lego-city-undercover-box.jpg"
+  },
+  {
+    "title": "LEGO Horizon Adventures",
+    "details": "Version 1.4.0",
+    "url": "../../../nintendo/lego-horizon-adventures.html",
+    "image": "../../../assets/images/nintendo/lego-horizon-adventures-box.png"
+  },
+  {
+    "title": "Legend Pokemon Arceus",
+    "details": "Version 1.1.0",
+    "url": "../../../nintendo/legend-pokemon-arceus.html",
+    "image": "../../../assets/images/nintendo/legend-pokemon-arceus-box.jpg"
+  },
+  {
+    "title": "Lego Ninjago, le film : Le Jeu vidéo",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/lego-ninjago-film.html",
+    "image": "../../../assets/images/nintendo/lego-ninjago-film-box.jpg"
+  },
+  {
+    "title": "Les Tortues Ninja – Le destin de Splinter",
+    "details": "Version 1.1.0",
+    "url": "../../../nintendo/tortues-ninja-splinter.html",
+    "image": "../../../assets/images/nintendo/tortues-ninja-splinter-box.webp"
+  },
+  {
+    "title": "Little Friends: Dogs and Cats",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/little-friends-dogs-and-cats.html",
+    "image": "../../../assets/images/nintendo/little-friends-dogs-and-cats-box.jpeg"
+  },
+  {
+    "title": "Luigi’s Mansion 2 HD",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/luigis-mansion-2-hd.html",
+    "image": "../../../assets/images/nintendo/luigis-mansion-2-hd-box.jpg"
+  },
+  {
+    "title": "Luigi’s Mansion 3",
+    "details": "Version 1.4.0 + DLC",
+    "url": "../../../nintendo/luigis-mansion-3.html",
+    "image": "../../../assets/images/nintendo/luigis-mansion-3-box.jpg"
+  },
+  {
+    "title": "Mario et Sonic aux Jeux Olympiques de Tokyo 2020",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/mario-sonic-jeux-olympiques-tokyo-2020.html",
+    "image": "../../../assets/images/nintendo/mario-sonic-jeux-olympiques-tokyo-2020-box.JPG"
+  },
+  {
+    "title": "Mario & Luigi: Brothership",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/mario-luigi-brothership.html",
+    "image": "../../../assets/images/nintendo/mario-luigi-brothership-box.jpg"
+  },
+  {
+    "title": "Mario Kart 8 Deluxe",
+    "details": "Version 3.0.3",
+    "url": "../../../nintendo/mario-kart-8-deluxe.html",
+    "image": "../../../assets/images/nintendo/mario-kart-8-deluxe-box.jpg"
+  },
+  {
+    "title": "Mario Party Superstars",
+    "details": "Version 1.1.0",
+    "url": "../../../nintendo/mario-party-superstars.html",
+    "image": "../../../assets/images/nintendo/mario-party-superstars-box.jpeg"
+  },
+  {
+    "title": "Mario + Rabbids Kingdom Battle",
+    "details": "Version 1.1.1",
+    "url": "../../../nintendo/mario-rabbids-kingdom-battle.html",
+    "image": "../../../assets/images/nintendo/mario-rabbids-kingdom-battle-box.png"
+  },
+  {
+    "title": "Mario Strikers Battle League",
     "details": "Version 1.3.0",
-    "url": "../../../nintendo/switch-sports.html",
-    "image": "../../../assets/images/nintendo/switch-sports-box.jpg"
+    "url": "../../../nintendo/mario-strikers-battle-league.html",
+    "image": "../../../assets/images/nintendo/mario-strikers-battle-league-box.jpg"
+  },
+  {
+    "title": "Mario Tennis Aces",
+    "details": "Version 1.3.0",
+    "url": "../../../nintendo/mario-tennis-aces.html",
+    "image": "../../../assets/images/nintendo/mario-tennis-aces-box.jpg"
+  },
+  {
+    "title": "Mario vs. Donkey Kong",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/mario-vs-donkey-kong.html",
+    "image": "../../../assets/images/nintendo/mario-vs-donkey-kong-box.jpg"
+  },
+  {
+    "title": "Metroid Dread",
+    "details": "Version 2.1.0",
+    "url": "../../../nintendo/metroid-dread.html",
+    "image": "../../../assets/images/nintendo/metroid-dread-box.jpg"
+  },
+  {
+    "title": "Miitopia",
+    "details": "Version 1.0.3",
+    "url": "../../../nintendo/miitopia.html",
+    "image": "../../../assets/images/nintendo/miitopia-box.jpg"
+  },
+  {
+    "title": "Minecraft",
+    "details": "Version 1.21.50",
+    "url": "../../../nintendo/minecraft.html",
+    "image": "../../../assets/images/nintendo/minecraft-box.jpg"
+  },
+  {
+    "title": "Monster Hunter Rise",
+    "details": "Version 16.0.2 + DLC",
+    "url": "../../../nintendo/monster-hunter-rise.html",
+    "image": "../../../assets/images/nintendo/monster-hunter-rise-box.jpeg"
+  },
+  {
+    "title": "Monster Hunter Generations Ultimate",
+    "details": "Version 1.4.0",
+    "url": "../../../nintendo/monster-hunter-generation-ultimate.html",
+    "image": "../../../assets/images/nintendo/monster-hunter-generation-ultimate-box.jpg"
+  },
+  {
+    "title": "Monster Hunter Stories",
+    "details": "Version 1.0.1 + DLC",
+    "url": "../../../nintendo/monster-hunter-stories.html",
+    "image": "../../../assets/images/nintendo/monster-hunter-stories-box.jpg"
+  },
+  {
+    "title": "My Hero One's Justice 2",
+    "details": "Version 1.1.5 + DLC",
+    "url": "../../../nintendo/my-hero-ones-justice-2.html",
+    "image": "../../../assets/images/nintendo/my-hero-ones-justice-2-box.jpg"
+  },
+  {
+    "title": "Naruto x Boruto Ultimate Ninja Storm Connections",
+    "details": "Version 1.5.0 + DLC",
+    "url": "../../../nintendo/naruto-boruto-ninja-storm-connections.html",
+    "image": "../../../assets/images/nintendo/naruto-boruto-ninja-storm-connections-box.png"
+  },
+  {
+    "title": "Naruto Shippuden Ultimate Ninja Storm 4: Road to Boruto",
+    "details": "Version 1.2.0 + DLC",
+    "url": "../../../nintendo/naruto-shippuden-ninja-storm-4.html",
+    "image": "../../../assets/images/nintendo/naruto-shippuden-ninja-storm-4-box.jpg"
+  },
+  {
+    "title": "Nerf Legends",
+    "details": "Version 1.1.5 + DLC",
+    "url": "../../../nintendo/nerf-legends.html",
+    "image": "../../../assets/images/nintendo/nerf-legends-box.jpg"
+  },
+  {
+    "title": "Night in the Woods",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/night-in-the-woods.html",
+    "image": "../../../assets/images/nintendo/night-in-the-woods-box.jpg"
+  },
+  {
+    "title": "New Pokémon Snap",
+    "details": "Version 2.0.1",
+    "url": "../../../nintendo/new-pokemon-snap.html",
+    "image": "../../../assets/images/nintendo/new-pokemon-snap-box.jpg"
+  },
+  {
+    "title": "New Super Mario Bros U Deluxe",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/new-super-mario-bros-u-deluxe.html",
+    "image": "../../../assets/images/nintendo/new-super-mario-bros-u-deluxe-box.jpg"
+  },
+  {
+    "title": "Nintendo Switch Sports",
+    "details": "Version 1.4.0",
+    "url": "../../../nintendo/nintendo-switch-sports.html",
+    "image": "../../../assets/images/nintendo/nintendo-switch-sports-box.jpg"
+  },
+  {
+    "title": "One Piece Pirate Warriors 4",
+    "details": "Version 1.8.0 + DLC",
+    "url": "../../../nintendo/one-piece-pirate-warriors-4.html",
+    "image": "../../../assets/images/nintendo/one-piece-pirate-warriors-4-box.jpg"
+  },
+  {
+    "title": "OneShot World Machine Edition",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/oneshot-world-machine.html",
+    "image": "../../../assets/images/nintendo/oneshot-world-machine-box.png"
+  },
+  {
+    "title": "One Piece Odyssey",
+    "details": "Version 1.0.0 + DLC",
+    "url": "../../../nintendo/one-piece-odyssey.html",
+    "image": "../../../assets/images/nintendo/one-piece-odyssey-box.jpg"
+  },
+  {
+    "title": "Ori and the Will of the Wisps",
+    "details": "Version 1.2.0",
+    "url": "../../../nintendo/ori-and-the-will-of-the-wisps.html",
+    "image": "../../../assets/images/nintendo/ori-and-the-will-of-the-wisps-box.jpg"
+  },
+  {
+    "title": "Paper Mario: The Thousand-Year Door",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/paper-mario-the-thousand-year-door.html",
+    "image": "../../../assets/images/nintendo/paper-mario-thousand-year-door-box.png"
+  },
+  {
+    "title": "Pikmin 2 HD",
+    "details": "Version 1.0.1",
+    "url": "../../../nintendo/pikmin-2-hd.html",
+    "image": "../../../assets/images/nintendo/pikmin-2-hd-box.jpg"
+  },
+  {
+    "title": "Pikmin 3 Deluxe",
+    "details": "Version 1.1.1",
+    "url": "../../../nintendo/pikmin-3-deluxe.html",
+    "image": "../../../assets/images/nintendo/pikmin-3-deluxe-box.jpg"
+  },
+  {
+    "title": "Pikmin 4",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/pikmin-4.html",
+    "image": "../../../assets/images/nintendo/pikmin-4-box.png"
+  },
+  {
+    "title": "Pokémon Bouclier",
+    "details": "Version 1.3.2 + DLC",
+    "url": "../../../nintendo/pokemon-bouclier.html",
+    "image": "../../../assets/images/nintendo/pokemon-bouclier-box.jpg"
+  },
+  {
+    "title": "Pokémon Diamant Étincelant",
+    "details": "Version 1.3.0",
+    "url": "../../../nintendo/pokemon-diamant-etincelant.html",
+    "image": "../../../assets/images/nintendo/pokemon-diamant-etincelant-box.jpg"
+  },
+  {
+    "title": "Pokémon Donjon Mystère : Équipe de secours DX",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/pokemon-donjon-mystere-dx.html",
+    "image": "../../../assets/images/nintendo/pokemon-donjon-mystere-dx-box.jpg"
+  },
+  {
+    "title": "Pokémon Écarlate",
+    "details": "Version 3.0.1 + DLC",
+    "url": "../../../nintendo/pokemon-ecarlate.html",
+    "image": "../../../assets/images/nintendo/pokemon-ecarlate-box.jpg"
+  },
+  {
+    "title": "Pokémon Épée",
+    "details": "Version 1.3.2 + DLC",
+    "url": "../../../nintendo/pokemon-epee.html",
+    "image": "../../../assets/images/nintendo/pokemon-epee-box.png"
+  },
+  {
+    "title": "Pokémon Let's Go Évoli",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/pokemon-lets-go-evoli.html",
+    "image": "../../../assets/images/nintendo/pokemon-lets-go-evoli-box.jpg"
+  },
+  {
+    "title": "Pokémon Let's Go Pikachu",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/pokemon-lets-go-pikachu.html",
+    "image": "../../../assets/images/nintendo/pokemon-lets-go-pikachu-box.jpg"
+  },
+  {
+    "title": "Pokémon Perle Scintillante",
+    "details": "Version 1.3.0",
+    "url": "../../../nintendo/pokemon-perle-scintillante.html",
+    "image": "../../../assets/images/nintendo/pokemon-perle-scintillante-box.png"
+  },
+  {
+    "title": "Pokémon Violet",
+    "details": "Version 3.0.1 + DLC",
+    "url": "../../../nintendo/pokemon-violet.html",
+    "image": "../../../assets/images/nintendo/pokemon-violet-box.jpg"
+  },
+  {
+    "title": "Pokken Tournament DX",
+    "details": "Version 1.3.3 + DLC",
+    "url": "../../../nintendo/pokken-tournament-dx.html",
+    "image": "../../../assets/images/nintendo/pokken-tournament-dx-box.jpg"
+  },
+  {
+    "title": "Princess Peach: Showtime!",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/princess-peach-showtime.html",
+    "image": "../../../assets/images/nintendo/princess-peach-showtime-box.jpg"
+  },
+  {
+    "title": "Rayman Legends Definitive Edition",
+    "details": "Version 1.0.2",
+    "url": "../../../nintendo/rayman-legends-definitive-edition.html",
+    "image": "../../../assets/images/nintendo/rayman-legends-definitive-edition-box.jpg"
+  },
+  {
+    "title": "Red Dead Redemption",
+    "details": "Version 1.0.4",
+    "url": "../../../nintendo/red-dead-redemption.html",
+    "image": "../../../assets/images/nintendo/red-dead-redemption-box.jpg"
+  },
+  {
+    "title": "Retour Détective Pikachu",
+    "details": "Version 1.0.0",
+    "url": "../../../nintendo/retour-detective-pikachu.html",
+    "image": "../../../assets/images/nintendo/retour-detective-pikachu-box.jpg"
+  },
+  {
+    "title": "Shin Megami Tensei V: Vengeance",
+    "details": "Version 1.3.0 + DLC",
+    "url": "../../../nintendo/shin-megami-tensei-v-vengeance.html",
+    "image": "../../../assets/images/nintendo/shin-megami-tensei-v-vengeance-box.PNG"
   }
 ];
